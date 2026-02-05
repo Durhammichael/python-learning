f244a084e925d08a863139389b62b137b995323a
Michael Durham <mddurham01@indianatech.net>
2026-02-05 02:18:20 -0500

    homework 1 assignment submission coding problem

---
+++ examscore.assn.py
@@ -0,0 +1,6 @@
+Student_name = input("First and last name here: ")
+Exam_score1 = int(input ("Enter exam score here: "))
+Exam_score2 = int(input ("Enter exam score here: "))
+Exam_score3 = int(input ("Enter exam score here: "))
+Average_score = (Exam_score1 + Exam_score2 + Exam_score3) / 3
+print (f"{Student_name}, your average score is: {Average_score:.2f}")
