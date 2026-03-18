35d5fc7c1c0683156847fdac038ab32dbc2fbce2
Michael Durham <mddurham01@indianatech.net>
2026-03-18 11:40:54 -0400

    MAKEUP MIDTERM

---
+++ CS1300_Makeup_Michael_Durham.py-2.py
@@ -0,0 +1,12 @@
+titles = ["python crash course", "clean code", "the pragmatic programmer", "code complete", "SICP"]
+Copies = [5, 3, 7, 2, 4]
+print(=== "library Catalog" ===)
+print(1. "python crash course      - 5 copies")
+print(2. "clean code               - 3 copies")
+print(3. "the pragmatic programmer - 7 copies")
+print(4. "code complete            - 2 copies")
+print(5. "SICP                     - 4 copies")
+print(============================)
+print("total books in library: 21")
+print("fewest books in library: code complete")
+print("most books in library: the pragmatic programmer") 
